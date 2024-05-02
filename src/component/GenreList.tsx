3bd5163f4d0d7a426b814725b8cf1d384a9f0237
itzhan <2802965114@qq.com>
2024-05-02 17:32:09 +0800

    显示加载图标在获取流派时

--- src/component/GenreList.tsx
+++ src/component/GenreList.tsx
@@ -1,21 +1,31 @@
-import { ListItem, List, Img, Text, HStack } from "@chakra-ui/react";
+import {
+  ListItem,
+  List,
+  Img,
+  Text,
+  HStack,
+  SkeletonText,
+} from "@chakra-ui/react";
 import UseGenre from "../hook/useGenre";
 import getCropImgUrl from "../services/img-url";
 
 const GenreList = () => {
-  const { data } = UseGenre();
+  const { data, isLoding, error } = UseGenre();
+  if (error) return null;
+
+  if (isLoding) return <SkeletonText></SkeletonText>;
 
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id} paddingY={'5px'}>
+        <ListItem key={genre.id} paddingY={"5px"}>
           <HStack>
             <Img
               src={getCropImgUrl(genre.image_background)}
               boxSize="32px"
               borderRadius={"8px"}
             ></Img>
-            <Text fontSize='large'>{genre.name}</Text>
+            <Text fontSize="large">{genre.name}</Text>
           </HStack>
         </ListItem>
       ))}
