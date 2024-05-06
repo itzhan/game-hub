5f0104dcd6408a0f9858ed67c3c8b7add9ab188f
itzhan <2802965114@qq.com>
2024-05-06 11:04:56 +0800

    重构：获取游戏数据的方式

--- src/component/GameGrid.tsx
+++ src/component/GameGrid.tsx
@@ -9,10 +9,10 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { error, data, isLoding } = useGames(gameQuery);
+  const { error, data, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -20,9 +20,9 @@ const GameGrid = ({ gameQuery }: Props) => {
       spacing={4}
       padding="10px"
     >
-      {isLoding &&
+      {isLoading &&
         skeletons.map((s) => <GameCradSkeleton key={s}></GameCradSkeleton>)}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCrad key={game.id} game={game}></GameCrad>
       ))}
     </SimpleGrid>
--- src/hook/useGames.ts
+++ src/hook/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import UseDatas from "./useData";
+import { FetchDatas } from "./useData";
+import apiClient from "../services/api-client";
 
 export interface Platforms {
   id: number;
@@ -8,26 +10,40 @@ export interface Platforms {
 }
 
 export interface Games {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platforms}[],
-    metacritic: number,
-    rating_top: number
-  }
-  
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platforms }[];
+  metacritic: number;
+  rating_top: number;
+}
 
-const UseGames = (
-  gameQuery: GameQuery) => 
-    UseDatas<Games>(
-      '/games', 
-  {
-    params: {
-      genres: gameQuery.genre?.id, 
-      parent_platforms: gameQuery.platform?.id,
-      ordering: gameQuery?.sortOrder,
-      search: gameQuery?.search
-      }
-    }, 
-    [gameQuery])
-export default UseGames
+const UseGames = (gameQuery: GameQuery) =>
+  useQuery<FetchDatas<Games>, Error>({
+    queryKey: [gameQuery, "games"],
+    queryFn: () =>
+      apiClient
+        .get<FetchDatas<Games>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery?.sortOrder,
+            search: gameQuery?.search,
+          },
+        })
+        .then((res) => res.data),
+        
+        staleTime: 1 * 60 * 60 * 1000 // 1h
+  });
+//   UseDatas<Games>(
+//     '/games',
+// {
+//   params: {
+//     genres: gameQuery.genre?.id,
+//     parent_platforms: gameQuery.platform?.id,
+//     ordering: gameQuery?.sortOrder,
+//     search: gameQuery?.search
+//     }
+//   },
+//   [gameQuery])
+export default UseGames;
